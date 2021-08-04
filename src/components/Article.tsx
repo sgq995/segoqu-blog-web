f5471f8651551c213a84d4128dd9a54d60d289ec
Sebastian G. Q <sgq995@gmail.com>
2021-08-04 15:29:56 -0500

    feat(article): Added animation when click

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,28 +1,46 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import PublicationDate from "./PublicationDate";
 import Summary from "./Summary";
 import Title from "./Title";
 
 interface ArticleProps {
+  id: number;
   title: string;
   date: number | Date;
   summary: string;
 }
 
 function Article({
+  id,
   title,
   date,
   summary
 }: ArticleProps): JSX.Element {
+  const [isAnimating, setIsAnimating] = React.useState(false);
+
+  const onClick = () => {
+    setIsAnimating(true);
+  }
+  const onAnimationEnd = () => {
+    setIsAnimating(false);
+  }
+
   return (
-    <article className="my-4">
-      <header className="mb-2">
-        <Title text={title} />
-        <PublicationDate date={date} />
-      </header>
-      
-      <Summary text={summary} />
-    </article>
+    <NavLink
+      to={`/posts/${id}`}
+      activeClassName="text-gray-500"
+      onClick={onClick}
+    >
+      <article className={`my-4 p-2 rounded-md hover:bg-gray-50 ${isAnimating ? 'animate-ping' : ''}`} onAnimationIteration={onAnimationEnd}>
+        <header className="mb-2">
+          <Title text={title} />
+          <PublicationDate date={date} />
+        </header>
+
+        <Summary text={summary} />
+      </article>
+    </NavLink>
   );
 }
 
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import Article from '../../components/Article';
+import Article from '../components/Article';
 
 function Home(): JSX.Element {
   const articles = new Array(5).fill(null).map(_ => ({
+    id: Math.floor(10 * Math.random()),
     title: 'Title',
     date: Date.now() - Math.floor(60 * 60 * 1000 * Math.random() + 60 * 60 * 1000),
     summary: 'Summary'
@@ -10,8 +11,9 @@ function Home(): JSX.Element {
 
   return (
     <section>
-      {articles.map(({ title, date, summary }) =>
+      {articles.map(({ id, title, date, summary }) =>
         <Article
+          id={id}
           title={title}
           date={date}
           summary={summary}
