51174d643acbefd61b2c34e3179da3394251bd87
Sebastian G. Q <sgq995@gmail.com>
2021-08-12 11:23:44 -0500

    chore(deps): Added lodash
    chore(config): Added mobile detection to improve UX
    
    chore(tailwind): Added top shadow for mobile navigation

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,14 @@
+const mobi = /Mobi/.test(navigator.userAgent);
+
 const config = {
   api: {
     baseURL: process.env.REACT_APP_API_BASEURL
+  },
+
+  device: {
+    isMobile: !!mobi,
+    isDesktop: !mobi
   }
 };
-
+console.log({config});
 export default config;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     extend: {
       fontFamily: {
         'montserrat': ['Montserrat']
-      }
+      },
+      boxShadow: {
+        'top-md': '0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)',
+        'top-lg': '0 -10px 15px -3px rgba(0, 0, 0, 0.1), 0 -4px 6px -2px rgba(0, 0, 0, 0.05)',
+      },
     },
   },
   variants: {
